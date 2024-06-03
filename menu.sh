e4e4b42ddeeec019f6234d496b4c4703d8b29fa8
Joshua Ross <josh@rossjm.dev>
2024-06-03 18:22:11 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -6,7 +6,7 @@
 
 
 # Use whiptail to create a menu
-CHOICE=$(whiptail --title "Media Server Installer" --menu "Choose a media server to install" 15 60 4 \
+CHOICE=$(whiptail --title "FlexMediaInstaller" --menu "Choose a media server to install" 15 60 4 \
 "Plex" "Install Plex Media Server" \
 "Emby" "Install Emby Server" \
 "Jellyfin" "Install Jellyfin Media Server" 3>&1 1>&2 2>&3)
